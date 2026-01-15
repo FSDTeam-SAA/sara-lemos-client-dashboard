1ad3e74f39faf63c331c739b1820b72d01e09d4d
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2026-01-16 04:39:22 +0600

    feat: introduce client dashboard with sidebar navigation, authentication, and pages for settings, subscription, social accounts, listings, and content generation.

---
+++ src/app/(dashboard)/saved-drafts/page.tsx
@@ -0,0 +1,8 @@
+import SavedDrafts from '@/components/Dashboard/SavedDrafts/SavedDrafts'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div><SavedDrafts /></div>
+  )
+}
---
+++ src/app/(dashboard)/settings/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import Settings from '@/components/Dashboard/Settings/Settings'
+
+export default function page() {
+  return (
+    <div><Settings /></div>
+  )
+}
